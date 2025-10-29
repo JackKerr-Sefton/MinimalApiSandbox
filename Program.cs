cd4f27044d64ad2e02d5b9e53d7319d3c1869d21
JackKerr <jack.kerr@sefton.gov.uk>
2025-10-29 15:46:48 +0000

    Test commit

--- Program.cs
+++ Program.cs
@@ -2,15 +2,12 @@ using MinimalApiSandbox;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 
